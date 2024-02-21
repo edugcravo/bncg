457e75027173aa40d91f6c5a613e7c2309d79ed9
edugcravo <edugcravo@hotmail.com>
2024-02-21 13:54:17 -0300

    Refactor login form in pin component

--- src/app/componentes/pin/pin.component.ts
+++ src/app/componentes/pin/pin.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-pin',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class PinComponent {
 
+  formulario!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private loginService: LoginService, private router: Router) { }
+
+  ngOnInit() {
+    this.formulario = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+
+  onSubmit() {
+    if (this.formulario.valid) {
+      console.log(this.formulario.value);
+      this.loginService.login(this.formulario.value).then((data: any) => {
+        console.log(data);
+        if(data.token){
+          this.router.navigate(['/favorecido']);
+        }else{
+          return
+        }
+
+      })
+    } else {
+      // Marque os campos como tocados para exibir os erros
+      this.formulario.markAllAsTouched();
+    }
+  }
 }
